613b629666c49239d7f7d38fb85b818672e95a81
Masayuki Suzuki <m.suzuki.fp@gmail.com>
2018-12-22 16:08:33 -0800

    Added checkbox for finising tasks

--- app.js
+++ app.js
@@ -1,26 +1,54 @@
 (function() {
     // DOMエレメントの取得
-    const todoInput = document.getElementById('todoTitle');
-    const todoAdd = document.getElementById('addToDo');
-    const todoList = document.getElementById('todoList');
-
-    // クリックした時に処理する関数
-    const clickHandlar = function(){
-        // input#todoTitleの値を取得する
-        const title = todoInput.value;
-
-        // アンチパターン
-        // todoList.innerHTML += ('<li>' + title + '</li>')
-
-        // liの要素を生成する(まだ反映はされない）
-        const newElm = document.createElement('li');
-        // 生成したliの中にinputの内容をプレーンテキストとして挿入
-        newElm.innerText = title;
-        // UIに作成したliを挿入する（ここで画面に反映される）
-        todoList.appendChild(newElm);
-
+    const doms = {
+        todoInput:  document.getElementById('todoTitle'),
+        todoAdd: document.getElementById('addToDo'),
+        todoList: document.getElementById('todoList'),
+        todoForm: document.querySelector('.todo-form'),
     }
 
-    todoAdd.addEventListener('click', clickHandlar);
+    // クリックした時に処理する関数
+    const submitHandlar = function(event){
+        if (
+            ( event.target.id === 'addToDo' || event.key === 'Enter' ) &&
+            !event.isComposing
+        ) {
+            // input#todoTitleの値を取得する
+            const title = doms.todoInput.value;
 
+            if (title) {
+                // checkbox用のinputの生成
+                const checkbox = document.createElement('input');
+                // inputのタイプ属性の設定
+                checkbox.setAttribute('type', 'checkbox');
+                checkbox.classList.add('todoDone')
+
+                // テキスト用のspanの生成
+                const textSpan = document.createElement('span');
+                // 生成したspanの中にinputの内容をプレーンテキストとして挿入
+                textSpan.innerText = title;
+
+                // liの要素を生成する(まだ反映はされない）
+                const newElm = document.createElement('li');
+                // UIに作成したliを挿入する（ここで画面に反映される）
+                doms.todoList.appendChild(newElm);
+            }
+        }
+    }
+
+    const todoClickHandlar = function(event) {
+        const target = event.target
+        if (target.classList.contains('todoDone')) {
+            if (target.checked) {
+                target.nextSibling.classList.add('done');
+            } else {
+                target.nextSibling.classList.remove('done');
+            }
+        }
+    }
+
+    doms.todoForm.addEventListener('click', submitHandlar);
+    doms.todoForm.addEventListener('keydown', submitHandlar);
+    doms.todoList.addEventListener('click', todoClickHandlar);
 })()
+
